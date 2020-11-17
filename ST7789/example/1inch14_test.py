3139eb1ee0e2a04469640c9ac6f9105840801484
Dmitry Brant <dbrant@wikimedia.org>
2020-11-16 19:34:31 -0500

    Add code for ST7789.

---
+++ ST7789/example/1inch14_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+import sys
+import time
+import logging
+import spidev as SPI
+sys.path.append("..")
+from lib import LCD_1inch14
+from PIL import Image,ImageDraw,ImageFont
+
+# Raspberry Pi pin configuration:
+RST = 27
+DC = 25
+BL = 18
+bus = 0
+device = 0
+logging.basicConfig(level=logging.DEBUG)
+try:
+    # display with hardware SPI:
+    #disp = LCD_1inch14.LCD_1inch14(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
+    disp = LCD_1inch14.LCD_1inch14()
+    # Initialize library.
+    disp.Init()
+    #disp.clear()
+
+    image = Image.open('bissel2.png')
+    disp.ShowImage(image)
+
+    disp.module_exit()
+except IOError as e:
+    logging.info(e)
+except KeyboardInterrupt:
+    disp.module_exit()
+    exit()
